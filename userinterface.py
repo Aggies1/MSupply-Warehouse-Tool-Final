c753ec316d8b526f4ea13a08b29d164129bd4827
Aggies1 <91496522+Aggies1@users.noreply.github.com>
2025-11-13 21:41:27 -0600

    Fix import statement for marcone warehouse analysis

--- userinterface.py
+++ userinterface.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # Import the function(s) from your other file
-from marconeapp import marcone_app.py
+from marconeapp import marcone_warehouse_analysis_app.py
 
 # --- Start of your website UI ---
 st.title("My Program Shell")
